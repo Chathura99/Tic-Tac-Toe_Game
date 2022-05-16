c643b5dd47c0b1a73b46a8bf03512a6430a5996c
Chathura <chathura.madura11@gmail.com>
2022-05-16 19:47:53 +0530

    view game history

---
+++ src/components/Moves/Moves.tsx
@@ -0,0 +1,38 @@
+import { SquareValue } from "../../types/types";
+import NextPlayer from "./NextPlayer";
+import "./moves.css";
+
+type moveProps = {
+    history: SquareValue[][] | null;
+    nextPlayer: String
+    timeTravel: (value: number) => void
+    winner: String | null
+}
+const Moves: React.FC<moveProps> = (props) => {
+    return (
+        <div>
+            {
+                props.winner ? <h2> {props.winner} You Won!!!</h2> :
+                    <div>
+                        <NextPlayer nextPlayer={props.nextPlayer} />
+                        <h3>Moves(History)</h3>
+                        <ol>
+                            <li onClick={() => props.timeTravel(-1)}>Go to game start</li>
+                            {/* conditional rendering */}
+
+                            {
+                                props.history?.map((item, index) => {
+                                    if (index == 0) return
+                                    return <li key={index} onClick={() => props.timeTravel(index)}>Go to #{index}</li>
+                                })
+                            }
+
+                        </ol>
+                    </div>
+            }
+        </div>
+
+    );
+};
+
+export default Moves;
