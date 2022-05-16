a6c1b9048a300ef784035096799793ecd4d8de60
Chathura <chathura.madura11@gmail.com>
2022-05-16 19:46:38 +0530

    create board component import square component

---
+++ src/components/Board/Board.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import { SquareValue } from "../../types/types";
+import Square from "../Square/Square";
+import "./board.css";
+
+type BoardProps = {
+    currentGame : SquareValue[]
+    squareClick : (value:number)=>void
+}
+
+const Board: React.FC<BoardProps> = (props) => {
+    // const [x,setX]=useState<number>(0);
+    // const incrementX = () => {
+    //     setX(x+1);
+    // }
+
+    const renderSquare = (value:number) =>{
+        // call from child(square)
+        return <Square clickSquareHandler={()=>{props.squareClick(value)}} value={props.currentGame[value]} />
+    }
+
+    
+    return (
+        <div className="board">
+            {renderSquare(0)}
+            {renderSquare(1)}
+            {renderSquare(2)}
+            {renderSquare(3)}
+            {renderSquare(4)}
+            {renderSquare(5)}
+            {renderSquare(6)}
+            {renderSquare(7)}
+            {renderSquare(8)}
+            
+            {/* <div><h1>x is {x}</h1>
+            <button onClick={incrementX}>+</button>
+            </div> */}
+        </div>
+    );
+};
+
+export default Board;
